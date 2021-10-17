8d51c78b05ff7001bc5d502f2ddf58599183f395
abyo <alexis.ollandfr@gmail.com>
2021-10-17 14:28:43 +0200

    Modify placeholder name to layton for client

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,5 @@
 import config from './config/options';
-import { PlaceClient } from './utils/PlaceClient';
+import { PlaceClient } from './utils/LaytonClient';
 
 const client = new PlaceClient(config);
 void client.start(); 
