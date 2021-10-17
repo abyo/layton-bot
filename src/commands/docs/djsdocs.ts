458fb854a6faebba2b8593d62b0e267b5ccf54c6
abyo <alexis.ollandfr@gmail.com>
2021-10-17 14:55:29 +0200

    Modify djsdocs command for easier usage

--- src/commands/docs/djsdocs.ts
+++ src/commands/docs/djsdocs.ts
@@ -4,16 +4,6 @@
 import { Command } from 'discord-akairo';
 import { Message } from 'discord.js';
 import fetch from 'node-fetch';
-import * as qs from 'querystring';
-
-const SOURCES = ['stable', 'master', 'rpc', 'collection'];
-
-interface DocsCommandArguments {
-	source: string;
-	force: boolean;
-	includePrivate: boolean;
-	query: string;
-}
 
 export default class DjsDocsCommand extends Command {
 	public constructor() {
@@ -22,59 +12,31 @@ export default class DjsDocsCommand extends Command {
 			description: {
 				content: 'Renvoie un embed de la documentation de djs',
 				usage: '<query>',
-				examples: ['TextChannel', 'Client', 'ClientUser#setActivity --src=master'],
+				examples: ['TextChannel', 'Client#guildMemberAdd', 'Guild'],
 			},
 			category: 'docs',
 			clientPermissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
 			userPermissions: ['SEND_MESSAGES'],
-			flags: ['--force', '-f', '--private', '-p'],
-			optionFlags: ['--default=', '--src='],
+			args: [
+				{
+					id: 'query',
+					prompt: {
+						start: (message: Message) => `${message.author}, qu'est ce que vous cherchez?`,
+					},
+					match: 'content',
+					type: (_, query) => (query ? encodeURIComponent(query.replace(/ /g, '-')) : null),
+				}
+			]
 		});
 	}
 
-	public *args() {
-		const defaultDocs = yield {
-			match: 'option',
-			flag: '--default=',
-		};
-
-		const source = yield {
-			match: 'option',
-			flag: '--src=',
-		};
-
-		const force = yield {
-			match: 'flag',
-			flag: ['--force', '-f'],
-		};
-
-		const includePrivate = yield {
-			match: 'flag',
-			flag: ['--private', '-p'],
-		};
-
-		const query = yield {
-			match: 'rest',
-			type: 'lowercase',
-			prompt: {
-				start: (message: Message) => `${message.author}, qu'est ce que vous cherchez?`,
-				optional: defaultDocs ? true : false,
-			},
-		};
-
-		return { defaultDocs, source, force, includePrivate, query };
-	}
-
-	public override async exec(message: Message, { source, force, includePrivate, query }: DocsCommandArguments) {
+	public override async exec(message: Message, { query }: {query: string}) {
 		message.delete();
-		const q = query.split(' ');
-		if (!SOURCES.includes(source)) source = 'stable';
-		const queryString = qs.stringify({ src: source, q: q.join(' '), force, includePrivate });
-		const res = await fetch(`https://djsdocs.sorta.moe/v2/embed?${queryString}`);
+		const res = await fetch(`https://djsdocs.sorta.moe/v2/embed?src=stable&q=${query}`);
 		const embed = await res.json();
     // TODO: find a way to split embeds (split is gone) - Util.splitMessage?
     if (query == 'client') return message.channel.send('Recherche pas assez spécifique!');
-		if (!embed) return message.reply('erreur!');
+		if (!embed) return message.reply('Erreur!');
     message.channel.send({ embeds: [embed] });
 	}
 }
