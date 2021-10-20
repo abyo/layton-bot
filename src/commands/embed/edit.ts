6775ba2fbbc5d4e2fb3efc7d4d2d7fe764976da3
abyo <alexis.ollandfr@gmail.com>
2021-10-20 13:44:39 +0200

    Add manual embeds commands

---
+++ src/commands/embed/edit.ts
@@ -0,0 +1,32 @@
+import { Command } from 'discord-akairo';
+import { Message, Snowflake } from 'discord.js';
+
+export default class EditCommand extends Command {
+	public constructor() {
+		super('edit', {
+			aliases: ['edit'],
+			category: 'embed',
+      channel: 'guild',
+			description: {
+				content: 'Edit an embed.',
+				usage: 'edit',
+				examples: ['edit']
+			},
+			ownerOnly: true,
+			clientPermissions: ['EMBED_LINKS']
+		});
+	}
+
+	public override async exec(message: Message): Promise<void> {
+    const embedToEdit = await message.channel.messages.fetch('900340365159178250');
+		
+    // Use Zira's builder: https://zira.gg/embedbuilder/
+    const embed = {
+			"color": 0,
+			"title": "LOL",
+			"description": "LOL"
+		};
+
+		embedToEdit.edit({ embeds: [embed] });
+	}
+}
