22451f765fcdd740783e8bf6eed53d28f08ab5b0
abyo <alexis.ollandfr@gmail.com>
2022-03-22 10:43:52 +0100

    Add presence to the bot

--- events/client/ready.js
+++ events/client/ready.js
@@ -6,6 +6,8 @@ module.exports = {
   async execute(client) {
     Logger.client("- prêt à être utilisé");
 
+    client.user.setPresence({ activities: [{ name: 'avec /help', type: 'PLAYING' }], status: 'dnd' });
+
     const devGuild = await client.guilds.cache.get('810091118401552395');
     devGuild.commands.set(client.commands.map(cmd => cmd));
   },
