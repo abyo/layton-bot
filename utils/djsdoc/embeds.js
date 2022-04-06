1734fd347c6fd46d18bded6527af3b2c394de4d2
Veauville Raphael <veauville.raphael@gmail.com>
2022-04-06 15:13:30 +0200

    Add spaces between properties, methods, and events

--- utils/djsdoc/embeds.js
+++ utils/djsdoc/embeds.js
@@ -28,19 +28,19 @@ function buildGeneralEmbed(parent, meta) {
   if (parent.props?.length) {
     description += "\n\n**Properties:**\n";
     parent.props.forEach((p, index) => {
-      description += `\`${p.name}\`${index + 1 < parent.props.length ? "," : ""}`;
+      description += `\`${p.name}\`${index + 1 < parent.props.length ? ", " : ""}`;
     });
   }
   if (parent.methods?.length) {
     description += "\n\n**Methods:**\n";
     parent.methods.forEach((m, index) => {
-      description += `\`${m.name}\`${index + 1 < parent.methods.length ? "," : ""}`;
+      description += `\`${m.name}\`${index + 1 < parent.methods.length ? ", " : ""}`;
     });
   }
   if (parent.events?.length) {
     description += "\n\n**Events:**\n";
     parent.events.forEach((e, index) => {
-      description += `\`${e.name}\`${index + 1 < parent.events.length ? "," : ""}`;
+      description += `\`${e.name}\`${index + 1 < parent.events.length ? ", " : ""}`;
     });
   }
   if (parent.meta) description += `\n\n[Source code](${meta.github + parent.meta.path + "/" + parent.meta.file + "#L" + parent.meta.line})`;
